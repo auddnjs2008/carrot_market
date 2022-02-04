a1407b98a7fd7732aff96ea53c0630a2d9922f07
M.W <auddnjs2008@naver.com>
2022-02-04 20:19:45 +0900

    prisma 셋팅, next js api 사용

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,10 +1,13 @@
+import { NextPage } from "next";
 import { useState } from "react";
+import Button from "../components/button";
+import Input from "../components/input";
 
 function cls(...classnames: string[]) {
   return classnames.join(" ");
 }
 
-export default function Enter() {
+const Enter: NextPage = () => {
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => setMethod("email");
   const onPhoneClick = () => setMethod("phone");
@@ -40,53 +43,39 @@ export default function Enter() {
             </button>
           </div>
         </div>
-        <form className="flex flex-col mt-8">
-          <label htmlFor="input" className="text-sm font-medium text-gray-700">
-            {method === "email" ? "Email address" : null}
-            {method === "phone" ? "Phone number" : null}
-          </label>
-          <div className="mt-1">
-            {method === "email" ? (
-              <input
-                id="input"
-                type="email"
-                className="appearance-none w-full px-3 py-2 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                required
-              />
-            ) : null}
-            {method === "phone" ? (
-              <div className="flex rounded-md shadow-sm">
-                <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none tex-sm">
-                  +82
-                </span>
-                <input
-                  id="input"
-                  type="number"
-                  className="appearance-none w-full px-3 py-2 border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                  required
-                />
-              </div>
-            ) : null}
-          </div>
-          <button className="mt-5 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-            {method === "email" ? "Get login link" : null}
-            {method === "phone" ? "Get one-time password" : null}
-          </button>
+        <form className="flex flex-col mt-8 space-y-4">
+          {method === "email" ? (
+            <Input name="email" label="Email address" type="email" required />
+          ) : null}
+          {method === "phone" ? (
+            <Input
+              name="phone"
+              label="Phone number"
+              type="number"
+              kind="phone"
+              required
+            />
+          ) : null}
+          {method === "email" ? <Button text={"Get login link"} /> : null}
+          {method === "phone" ? (
+            <Button text={"Get one-time password"} />
+          ) : null}
         </form>
+
         <div className="mt-8">
           <div className="relative">
             <div className="absolute w-full border-t border-gray-300" />
-            <div className="relative -top-3 text-center">
+            <div className="relative -top-3 text-center ">
               <span className="bg-white px-2 text-sm text-gray-500">
                 Or enter with
               </span>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-3 m-2">
+          <div className="grid grid-cols-2 mt-2 gap-3">
             <button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
               <svg
                 className="w-5 h-5"
-                arai-hidden="true"
+                aria-hidden="true"
                 fill="currentColor"
                 viewBox="0 0 20 20"
               >
@@ -112,4 +101,6 @@ export default function Enter() {
       </div>
     </div>
   );
-}
+};
+
+export default Enter;
